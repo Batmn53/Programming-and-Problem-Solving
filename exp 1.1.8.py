35a5cf8d18570075bee3794eb250bfc9ec6172a0
Batmn53 <codingfanatic53@gmail.com>
2025-05-07 16:38:11 +0530

    Create exp 1.1.8.py

---
+++ exp 1.1.8.py
@@ -0,0 +1,8 @@
+#Write a Python program to perform union, intersection and difference operations on Set A and Set B.
+a = list(map(int,input("Set A: ").split()))
+A = set(a)
+b = list(map(int,input("Set B: ").split()))
+B = set(b)
+print(f"Union:  {A | B}")
+print(f"Intersection:  {A & B}")
+print(f"Difference:  {A - B}")
