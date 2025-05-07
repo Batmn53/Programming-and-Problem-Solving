75b9cf536ee09b5bdf7c768bee8aac12d140e346
Batmn53 <codingfanatic53@gmail.com>
2025-05-07 16:45:07 +0530

    Create exp 1.1.13.py

---
+++ exp 1.1.13.py
@@ -0,0 +1,17 @@
+'''
+Write a Python program to print the following pattern.
+
+Sample Input and Output:
+Enter a number : 6
+* * * * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+'''
+n = int(input("Enter a number : "))
+for i in range(n, 0, -1):
+	for j in range(i, 0, -1):
+		print("* ",end="")
+	print("")
